2537f34b21bd4c23f7ff7125c1b7f8fba1f18b63
Shivam <kushwahashivam050@gmail.com>
2024-04-26 20:56:54 +0530

    updated

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-500 py-8">
+        <footer className="bg-gray-400 py-8">
             <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
                     {/* Company Section */}
